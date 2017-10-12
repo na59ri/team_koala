99f5bb862ff767200b880c2d3de05fa212408dbc
na59ri <32233567+na59ri@users.noreply.github.com>
2017-10-13 05:53:12 +0900

    Input interview/story dammy data

--- www/js/interview_data.js
+++ www/js/interview_data.js
@@ -1,93 +1,93 @@
 // parameter設定
 var interviewData = [{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"images/woman_test1.png",
     "questionid":'1',
-    "question":"質問です１",
+    "question":"あなたは意中のB君とLineを交換しました。最初にどのようにLineをしますか?",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"B君スタートで礼儀正しく最初に自己紹介をし合う（初めまして〜です。出身は〜で）"},
+        {"ansid":'2',"ans":"B君スタートでフランクな短文メッセージを送り合い、徐々にお互いのことを知っていく（こんにちは、よろしく）"},
+        {"ansid":'3',"ans":"自分からメッセージを送る"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"images/woman_test3.png",
     "questionid":'2',
-    "question":"質問です２",
+    "question":"あなたはLineで髪を切った時の自撮り写真を送りました。もしB君が前の髪型のほうが好みだとするとどのように返してほしいですか？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"嘘をついて今の髪型を褒めてほしい"},
+        {"ansid":'2',"ans":"正直に前の髪型が好みだと伝えてほしい"},
+        {"ansid":'3',"ans":"ARで合成して、とりあえずふざけてほしい"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"images/story2.png",
     "questionid":'3',
-    "question":"質問です３",
+    "question":"B君と電話をしますか？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"Lineを続ける"},
+        {"ansid":'2',"ans":"理由をつくって電話をかけてほしい"},
+        {"ansid":'3',"ans":"電話をしたいと伝えて電話をかけてほしい"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"images/story3.png",
     "questionid":'4',
-    "question":"質問です４",
+    "question":"B君と電話中です。あなたは悩み事で落ち込んでおり、その話をB君にしました。どのような対応が嬉しいですか？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"話を聞いて、解決策を提案する"},
+        {"ansid":'2',"ans":"とりあえず、うんうんと聞いてくれる"},
+        {"ansid":'3',"ans":"テンションを上げて自虐ネタで元気を出させようとしてくれる"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"images/story4.png",
     "questionid":'5',
-    "question":"質問です５",
+    "question":"デートに行くことになりました。どんなデートが良いですか？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"朝から一日中、遊園地や水族館などがっつりデート"},
+        {"ansid":'2',"ans":"昼から近場でランチやお買い物デート"},
+        {"ansid":'3',"ans":"お気に入りアニメの聖地巡礼に連れて行かれる"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"images/story5.png",
     "questionid":'6',
-    "question":"質問です６",
+    "question":"B君とランチに行きます。あなたは待ち合わせに遅刻しています。B君にどのように伝えますか？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"伝えない。とりあえず待っていてほしい"},
+        {"ansid":'2',"ans":"今どこにいるかLineか電話で聞かれたら答える"},
+        {"ansid":'3',"ans":"自宅サーバにログインして昨日見逃したアニメを見といてほしい"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"images/story4.png",
     "questionid":'7',
-    "question":"質問です７",
+    "question":"B君とカフェに来ています。話が盛り上がっていません。B君にしてほしい対応は？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"いろいろ質問してほしい"},
+        {"ansid":'2',"ans":"B君の趣味などの話を聞きたい"},
+        {"ansid":'3',"ans":"コミケの話をしたい"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"images/story7.png",
     "questionid":'8',
-    "question":"質問です８",
+    "question":"B君と花火に来ています。あなたは買った唐揚げを落としてしまいました。嬉しい対応は？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"優しく新しいものを買ってくれる"},
+        {"ansid":'2',"ans":"落としたことをいじって笑いに変えてくれる"},
+        {"ansid":'3',"ans":"とりあえずコミケの話をしてくれる"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"images/story8.png",
     "questionid":'9',
-    "question":"質問です９",
+    "question":"ディナーを食べ終わったあと、お会計での理想の形は？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"割り勘"},
+        {"ansid":'2',"ans":"全額おごってもらう"},
+        {"ansid":'3',"ans":"全額おごらせてもらう"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"images/story9.png",
     "questionid":'10',
-    "question":"質問です１０",
+    "question":"デートの終わりに二人きりで港で船を眺めていい雰囲気になっています。どうしますか",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"何もせず、次回に持ち越す"},
+        {"ansid":'2',"ans":"好きであることを伝える。または匂わせる"},
+        {"ansid":'3',"ans":"とりあえず抱きしめられたい"}
     ]
 },
 ];
--- www/js/story_data.js
+++ www/js/story_data.js
@@ -1,93 +1,93 @@
 // parameter設定
 var interviewData = [{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"https://s3-ap-northeast-1.amazonaws.com/dev-box-bitriver-s3/red2017/cosplay-851050_640.jpg",
     "questionid":'1',
-    "question":"質問です１",
+    "question":"あなたは意中のAさんとLineを交換しました。最初にどのようなLineを送りますか？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"礼儀正しく最初に自己紹介を述べる（初めまして〜です。出身は〜で）"},
+        {"ansid":'2',"ans":"フランクな短文メッセージを送る（こんにちは、よろしく）"},
+        {"ansid":'3',"ans":"メッセージを待つ"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"https://s3-ap-northeast-1.amazonaws.com/dev-box-bitriver-s3/red2017/scissors-33117_640.png",
     "questionid":'2',
-    "question":"質問です２",
+    "question":"AさんからLineで髪を切った時の自撮り写真が送られてきました。しかし前の髪型のほうが好みです。どのようにLineを返しますか？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"嘘をついて今の髪型を褒める"},
+        {"ansid":'2',"ans":"正直に前の髪型が好みだと伝える"},
+        {"ansid":'3',"ans":"ARで合成する"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"https://s3-ap-northeast-1.amazonaws.com/dev-box-bitriver-s3/red2017/moe-595960_640.png",
     "questionid":'3',
-    "question":"質問です３",
+    "question":"Aさんに電話をかけますか？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"Lineを続ける"},
+        {"ansid":'2',"ans":"理由をつくって電話をかける"},
+        {"ansid":'3',"ans":"電話をしたいと伝えて電話をかける"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"https://s3-ap-northeast-1.amazonaws.com/dev-box-bitriver-s3/red2017/girl-1350245_640.jpg",
     "questionid":'4',
-    "question":"質問です４",
+    "question":"電話先でAさんが悩み事で落ち込んでいます。どのように対応しますか？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"話を聞いてあげて解決策を提案する"},
+        {"ansid":'2',"ans":"とりあえず、うんうんと聞いてあげる"},
+        {"ansid":'3',"ans":"テンションを上げて自分のニート歴を伝え、元気を出させる"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"https://s3-ap-northeast-1.amazonaws.com/dev-box-bitriver-s3/red2017/casal-1818171_640.png",
     "questionid":'5',
-    "question":"質問です５",
+    "question":"デートに行くことになりました。何時からどこへ誘いますか？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"朝から一日中、遊園地や水族館などがっつりデート"},
+        {"ansid":'2',"ans":"昼から近場でランチやお買い物デート"},
+        {"ansid":'3',"ans":"お気に入りアニメの聖地巡礼に誘う"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"https://s3-ap-northeast-1.amazonaws.com/dev-box-bitriver-s3/red2017/moe-573290_640.png",
     "questionid":'6',
-    "question":"質問です６",
+    "question":"Aさんとランチに行きます。Aさんが待ち合わせ時間を過ぎても来ません。どうしますか？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"とりあえず気長に待つ"},
+        {"ansid":'2',"ans":"今どこにいるかLineか電話で聞く"},
+        {"ansid":'3',"ans":"自宅サーバにログインして昨日見逃したアニメを見る"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"https://s3-ap-northeast-1.amazonaws.com/dev-box-bitriver-s3/red2017/casal-1818171_640.png",
     "questionid":'7',
-    "question":"質問です７",
+    "question":"Aさんとカフェに来ています。話が盛り上がっていません。なんの話題で盛り上げますか？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"Aさんのことを聞き、それにリアクションする"},
+        {"ansid":'2',"ans":"自分の趣味などの話をする"},
+        {"ansid":'3',"ans":"コミケの話をする"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"https://s3-ap-northeast-1.amazonaws.com/dev-box-bitriver-s3/red2017/lovers-2761553_640.png",
     "questionid":'8',
-    "question":"質問です８",
+    "question":"Aさんと花火に来ています。Aさんが買った唐揚げを落としてしまいました。どのように対応しますか？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"優しく新しいものを買ってあげる"},
+        {"ansid":'2',"ans":"落としたことをいじって笑いに変える"},
+        {"ansid":'3',"ans":"とりあえずコミケの話をする"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"https://s3-ap-northeast-1.amazonaws.com/dev-box-bitriver-s3/red2017/moe-595956_640.png",
     "questionid":'9',
-    "question":"質問です９",
+    "question":"ディナーを食べ終わったあと、Aさんが半額だそうとしています。どうしますか？",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"半額受け取る"},
+        {"ansid":'2',"ans":"受け取らず全額おごる"},
+        {"ansid":'3',"ans":"全額求める"}
     ]
 },{
-    "image":"http://onlineschoolmanagementsoftware.com/wp-content/uploads/2013/10/4-15.jpg",
+    "image":"https://s3-ap-northeast-1.amazonaws.com/dev-box-bitriver-s3/red2017/sydney-2148908_640.jpg",
     "questionid":'10',
-    "question":"質問です１０",
+    "question":"デートの終わりに二人きりで港で船を眺めていい雰囲気になっています。どうしますか",
     "anslist":[
-        {"ansid":'1',"ans":"あああ"},
-        {"ansid":'2',"ans":"いいい"},
-        {"ansid":'3',"ans":"ううう"}
+        {"ansid":'1',"ans":"何もせず、次回に持ち越す"},
+        {"ansid":'2',"ans":"好きであることを伝える。または匂わせる。"},
+        {"ansid":'3',"ans":"とりあえず抱きしめる"}
     ]
 },
 ];
